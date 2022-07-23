09d4a61a1dbea8269d5004babdb23d240f19a3ff
bamsi <hibamsi@gmail.com>
2022-07-23 05:32:00 +0300

    add method decode_message

--- morse_code.rb
+++ morse_code.rb
@@ -40,5 +40,26 @@
 
 def decode_char(character)
   @morse_character = @morse_code.invert[character]
-  @morse_character.upcase
+  return @morse_character.upcase
+end
+
+def decode_word(word)
+  split_characters = word.split(' ')
+  result = ''
+  split_characters.each do |value| 
+    result.concat(decode_char(value))
+  end
+  return result
+end
+
+def decode_message(string)
+  split_string = string.split('   ')
+  result = ''
+  split_string.each do |value| 
+    result.concat(decode_word(value))
+    if value != split_string.last 
+      result.concat(' ')
+    end
+  end
+  return result
 end
