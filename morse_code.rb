0fafc86b902ca264a6a64b903b777cc6eb57620b
bamsi <hibamsi@gmail.com>
2022-07-23 06:03:58 +0300

    fix linters

--- morse_code.rb
+++ morse_code.rb
@@ -40,26 +40,24 @@
 
 def decode_char(character)
   @morse_character = @morse_code.invert[character]
-  return @morse_character.upcase
+  @morse_character.upcase
 end
 
 def decode_word(word)
-  split_characters = word.split(' ')
+  split_characters = word.split
   result = ''
-  split_characters.each do |value| 
+  split_characters.each do |value|
     result.concat(decode_char(value))
   end
-  return result
+  result
 end
 
 def decode_message(string)
   split_string = string.split('   ')
   result = ''
-  split_string.each do |value| 
+  split_string.each do |value|
     result.concat(decode_word(value))
-    if value != split_string.last 
-      result.concat(' ')
-    end
+    result.concat(' ') if value != split_string.last
   end
-  return result
+  result
 end
